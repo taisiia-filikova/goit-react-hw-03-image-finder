79eda552018709795d9a3b9c2d4a7c60d90f824c
taisiia-filikova <filikova@gmail.com>
2020-12-19 16:32:24 +0200

    Добавляет  модерн нормалайз

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
